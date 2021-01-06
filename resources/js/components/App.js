db7ec79cec3e85ac46eb9251535307a620c4d82b
vazgen1994 <vazgen.makaryan94@gmail.com>
2021-01-06 20:27:58 +0300

    add components to App component

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,12 +1,29 @@
 import React, {Component} from 'react';
+import SignUp from "./signUp/SignUp";
+import SignIn from "./signIn/SignIn";
+import Header from "./header/Header";
+import Home from "./home/Home";
+import Profile from "./profile/Profile";
+import FrontPage from "./frontPage/FrontPage"
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
 
 export default class App extends Component {
-	render() {
-		return (
-			<div>
-				Hello World
-			</div>	
-		);
-	}
+
+  render() {  
+    const login = localStorage.getItem("isLoggedIn");
+
+    return (
+      <div className="App">
+          <Router>
+            <Header />
+            <Route  path="/" exact component={FrontPage}></Route>     
+            <Route  path="/sign-up" component={ login ? Home : SignUp }></Route>
+            <Route path="/sign-in" component={SignIn}></Route>
+            <Route path="/home" component={Home}></Route>
+            <Route  path="/profile" component={Profile}></Route>       
+          </Router>
+      </div>
+    );
+  }
 }
