61830f87e716572f0a6ae39102a51a2825ac2a9d
vazgen1994 <vazgen.makaryan94@gmail.com>
2021-01-07 14:19:03 +0300

    in header add context logic

--- resources/js/components/header/Header.js
+++ resources/js/components/header/Header.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 import { NavLink, Redirect } from "react-router-dom";
+import withDataContext from '../../context/consumers/DataConsumer';
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 
 
-export default class Header extends Component {
+
+class Header extends Component {
 
 	constructor(props){
 		super(props);
@@ -14,12 +16,13 @@ export default class Header extends Component {
 
 	}
 
+
 	handleLogout = () => {
 		const token  = localStorage.getItem('token');
 		axios.post("http://social.loc/api/logout", {}, { headers: {"Authorization" : `Bearer ${token}`} })
 			.then((response) => {
 				localStorage.clear();
-				this.setState({isLoggout: response.data.status === 200});
+				this.setState({isLoggout: response.data.status === 200});				
 			})
 	}
 
@@ -37,34 +40,26 @@ export default class Header extends Component {
 			  <div className="collapse navbar-collapse justify-content-between" id="navbarTogglerDemo01">
 			    <NavLink to="/" className="navbar-brand">Social Network </NavLink>
 			    <ul className="navbar-nav  mt-2 mt-lg-0">
-			    {!login && (
-				    <React.Fragment>
-					      <li className="nav-item active">
-					        <NavLink to="/sign-in" className="navbar-brand">Sign In </NavLink>
-					      </li>
-					      <li className="nav-item">
-					        <NavLink to="/sign-up" className="navbar-brand">Sign Up </NavLink>
-					      </li>
-			      	</React.Fragment>
-			      )
+			    {this.props.isLogedIn && (
+					<>
+						<li className="nav-item">
+						<NavLink to="/home" className="navbar-brand">Home </NavLink>
+						</li>
+						<li className="nav-item">
+								<NavLink to="/profile" className="navbar-brand">Profile </NavLink>
+						</li>
+						<li className="nav-item">
+								<Button className="btn btn-success" onClick={this.handleLogout}>Logout</Button>
+						</li>
+					</>
+				)
 			      }
-			      {login && (
-				      <React.Fragment>
-				      	  <li className="nav-item">
-				        	<NavLink to="/home" className="navbar-brand">Home </NavLink>
-					      </li>
-					      <li className="nav-item">
-					        	<NavLink to="/profile" className="navbar-brand">Profile </NavLink>
-					      </li>
-					       <li className="nav-item">
-					        	<Button className="btn btn-success" onClick={this.handleLogout}>Logout</Button>
-					      </li>
-				      </React.Fragment>
-			      )
-			      }
+			      
 			    </ul>
 			  </div>
 			</nav>
 		);
 	}
 }
+
+export default withDataContext(Header, ['isLogedIn', 'toggleLogedIn'])
