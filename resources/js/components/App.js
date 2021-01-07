67087f8cf8dfab68da2b475cba1012f0572375ba
vazgen1994 <vazgen.makaryan94@gmail.com>
2021-01-07 14:19:36 +0300

    add context logic to App and index files

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,24 +1,24 @@
 import React, {Component} from 'react';
+import withDataContext from '../context/consumers/DataConsumer';
 import SignUp from "./signUp/SignUp";
 import SignIn from "./signIn/SignIn";
 import Header from "./header/Header";
 import Home from "./home/Home";
 import Profile from "./profile/Profile";
-import FrontPage from "./frontPage/FrontPage"
 import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
 
-export default class App extends Component {
+class App extends Component {
 
   render() {  
-    const login = localStorage.getItem("isLoggedIn");
+    const login = this.props.isLogedIn;
 
     return (
       <div className="App">
           <Router>
             <Header />
-            <Route  path="/" exact component={FrontPage}></Route>     
-            <Route  path="/sign-up" component={ login ? Home : SignUp }></Route>
+            <Route  path="/" exact component={SignIn}></Route>     
+            <Route  path="/sign-up" component={SignUp }></Route>
             <Route path="/sign-in" component={SignIn}></Route>
             <Route path="/home" component={Home}></Route>
             <Route  path="/profile" component={Profile}></Route>       
@@ -27,3 +27,5 @@ export default class App extends Component {
     );
   }
 }
+
+export default withDataContext(App, ['isLogedIn'])
--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import DataProvider from '../context/providers/DataProvider';
 import App from './App';
 
 if (document.getElementById('root1')) {
-    ReactDOM.render(<App />, document.getElementById('root1'));
+    ReactDOM.render(
+    <DataProvider>
+        <App />
+    </DataProvider>,
+    document.getElementById('root1'));
 }
