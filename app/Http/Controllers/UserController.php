4421210020d888c612d3d3fa7c7017d629251ecc
vazgen1994 <vazgen.makaryan94@gmail.com>
2021-01-07 18:38:10 +0300

    move auth logic from UserController and UserService

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,49 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Services\UserService;
-use App\User;
 
 class UserController extends Controller
 {
-	
-
-    public function signIn(UserService $userService, Request $request){
-
-    	$validator              =        Validator::make($request->all(), [
-            "email"             =>          "required|email",
-            "password"          =>          "required|min:8"
-        ]);
-
-        if($validator->fails()) {
-            return response()->json(["status" => "failed", "message" => "validation_error", "errors" => $validator->errors()]);
-        }
-
-        return response()->json($userService->signIn($request));
-
-
-
-    }
-
-    public function signUp(UserService $userService, Request $request){
-
-
-      	$validator              =        Validator::make($request->all(), [
-            "name"              =>          "required",
-            "surname"           =>          "required",
-            "email"             =>          "required|email",
-            "password"          =>          "required|min:8"
-        ]);
-
-        if($validator->fails()) {
-            return response()->json(["status" => "failed", "message" => "validation_error", "errors" => $validator->errors()]);
-        }
-
-        return response()->json($userService->signUp($request));
-    }
-
-    public function logOut(Userservice $userService){
-        return response()->json($userService->logout());
-    }
 
     public function updateProfile(Userservice $userService, Request $request){
 
--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -3,7 +3,6 @@
 namespace App\Services;
 
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
 class UserService
@@ -14,76 +13,6 @@ class UserService
 	  'pending' => 1,
 	  'rejected' => 2
 	];
-	
-
-	public function signIn($requestData) {
-
-		$credentials = $requestData->only('email', 'password');
-		
-		if (Auth::attempt($credentials)) {
-			$user = Auth::user();
-			$token = $user->createToken($user->email.'-'.now());					
-            return [
-            	'status' => $this->status_code,
-            	'success' =>	true,
-            	'message' => 'You have logged in successfully',
-            	'user' => $user,
-            	'token' => $token->accessToken
-            ];
-        }else {
-        	return [
-            	'status' => 'failed',
-            	'success' =>	false,
-            	'message' => 'Wrong credentials'
-            ];
-        }
-	}
-
-	public function signUp($requestData) {
-	  $existUser = User::where('email', $requestData->email)->first();	 
-
-	  if(is_null($existUser)) {
-	  	$userData = array(
-	  		'name' => $requestData->name,
-	  		'surname' => $requestData->surname,
-	  		'email' => $requestData->email,
-	  		'password' => Hash::make($requestData->password)
-	  	);
-
-	  	$user = User::create($userData);
-
-	  	if(!is_null($user)){
-	  		return array(
-	  			'status' => $this->status_code,
-            	'success' =>	true,
-            	'message' => 'Registration completed successfully',
-            	'data' => $user
-	  		);
-	  	}else {
-	  		return array(
-	  			'status' => 'failed',
-            	'success' =>	false,
-            	'message' => 'Failed to register'
-	  		);
-	  	}
-	  }else {
-	  		return array(
-	  			'status' => 'failed',
-            	'success' =>	false,
-            	'message' => 'Email already registered'
-	  		);
-	  }
-	}
-
-	public function  logout() {
-		if (Auth::user()) {
-	        $token = Auth::user()->token();
-	        $token->revoke();
-	        return array(
-  				'status' => 200
-  			);
-      }
-	}
 
 	public function updateProfile($requestData) {
 
